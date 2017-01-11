85057397d42fbeb5521f4f8e78beb80ad3f80450
Abdul Rahaman Shah <a.rahmanshah@gmail.com>
2017-01-11 07:23:50 +0300

    initial commit

---
+++ index.android.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import OneSignal from 'react-native-onesignal';
+
+OneSignal.configure({
+    onIdsAvailable: function(device) {
+        console.log('UserId = ', device.userId);
+        console.log('PushToken = ', device.pushToken);
+    },
+  onNotificationOpened: function(message, data, isActive) {
+      console.log('MESSAGE: ', message);
+      console.log('DATA: ', data);
+      console.log('ISACTIVE: ', isActive);
+      alert(message);
+      // Do whatever you want with the objects here
+      // _navigator.to('main.post', data.title, { // If applicable
+      //  article: {
+      //    title: data.title,
+      //    link: data.url,
+      //    action: data.actionSelected
+      //  }
+      // });
+  }
+})
+
+export default class MarcomJo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('MarcomJo', () => MarcomJo);
